ca20558415daa0910e3e6c6b87e5edff2ba0960e
Sirsergiomr <sirsergio.mr@gmail.com>
2020-11-15 14:43:13 +0100

    Clase Objeto Cliente y Alta mas Baja del cliete por parte de un Vendedor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 lista_empleados = []
-
+lista_clientes = []
 class Empleado():
     def __init__(self, nombre, apellidos,DNI,direccion,salario,supervisor):
         self.nombre = nombre
@@ -32,16 +32,37 @@ class CocheEmpresa():
     def __init__(self, matricula, marca, modelo):
         self.matricula = matricula
         self.marca = marca
-
+        self.modelo = modelo
+    def Imprimir(self):
+        print("|Matricula: ",self.matricula,"| Marca: ",self.marca,"| Modelo: ",self.modelo)
 class Vendedor(Empleado):
-    def __init__(self,telefono,areaVenta,listadeclientes,comision,incremento):
+    def __init__(self,cocheDeEmpresa,telefono,areaVenta,listadeclientes,comision,incremento):
         self.telefono = telefono
         self.areaVenta = areaVenta
         self.listaclientes = listadeclientes
         self.comision = comision
         self.incremento = incremento
+        self.cocheDeEmpresa = cocheDeEmpresa
     def Imprimir(self):
         print("|Telefono: ",self.telefono,"| Area de Venta", self.areaVenta,self.listaclientes,self.comision,self.incremento)
+
+    def AltaCliente(self):
+        Nombre = input("Ingrese Nombre del Cliente >>")
+        DNI = input("Ingrese DNI")
+        Nuevo_Cliente = Cliente(Nombre, DNI)
+        lista_clientes.append(Nuevo_Cliente)
+    def BajaCliente(self):
+        DNI = input("DNI del CLiente que quiere dar de Baja >>")
+
+        for Cliente in lista_clientes:
+            if Cliente.DNI == DNI:
+                lista_clientes.remove(Cliente)
+class Cliente():
+    def __init__(self,Nombre, DNI):
+        self.Nombre = Nombre
+        self.DNI = DNI
+    def Imprimir(self):
+        print("|Nombre: ",self.Nombre,"| DNI: ",self.DNI,"|")
 
 if __name__ == '__main__':
     NuevoSupervisor = Supervisor("Empleado 1","Lopez",4654654,"Calle San Marcos nº15",1500,"nulo")
